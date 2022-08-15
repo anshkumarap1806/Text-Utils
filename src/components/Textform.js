a5b3fb0810f4b014730a0542ac530c8b71cee052
anshkumarap1806 <102404447+anshkumarap1806@users.noreply.github.com>
2022-08-15 23:30:37 +0530

    Update Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -117,7 +117,7 @@ export default function Textform(props) {
     </div>
     <div className="container mt-4" style={{color:props.mode==='dark'?'white':'black'}}>
       <h1>Your Text Summary</h1>
-      <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} word and {text.length} character</p>
+      <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} word and {text.length} character</p>
       <p>{0.008*text.split(" ").filter((element)=>{return element.length !==0}).length} Minutes to read</p>
       <h2>Preview</h2>
       <p>{text}</p>
